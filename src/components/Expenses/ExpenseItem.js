e140f1ddcaaeca364db3e3c01889e02cb8545b36
jeiwong <weijwong@gmail.com>
2021-11-17 14:36:25 +0800

    Added hover and active on expenses item

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,21 @@
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
+import Button from "../UI/Button";
 import React from "react";
 
-const ExpenseItem = ({ title, amount, date }) => {
+const ExpenseItem = ({ title, amount, date, active }) => {
   return (
     <li>
-      <Card className="expense-item">
+      <Card className={`expense-item ${active ? "active" : ""}`}>
         <ExpenseDate date={date} />
         <div className="expense-item__description">
           <h2>{title}</h2>
-          <div className="expense-item__price">RM{amount}</div>
+          <div style={{ display: "flex" }}>
+            {active && <Button className="alternative">Delete</Button>}
+            {active && <Button className="alternative">Edit</Button>}
+            <div className="expense-item__price">RM{amount}</div>
+          </div>
         </div>
       </Card>
     </li>
--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpenseList.css";
 
 const ExpenseList = ({ item }) => {
+  const [activeIndex, setActiveIndex] = useState();
   if (item.length === 0) {
     return <h2 className="expenses-list__fallback">No expenses found.</h2>;
   }
 
+  const activeIndexHandler = (index) => {
+    setActiveIndex(prevIndex => prevIndex === index ? '' : index);
+  };
+
   return (
     <ul className="expenses-list">
-      {item.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
+      {item.map((expense, index) => (
+        <div key={index}  onClick={()=>activeIndexHandler(index)}>
+          <ExpenseItem
+            active={activeIndex === index}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        </div>
       ))}
     </ul>
   );
