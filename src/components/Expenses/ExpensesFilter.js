e0118513c613b8a5a9866269e1acef0ab735ff81
jeiwong <weijwong@gmail.com>
2021-11-17 20:45:51 +0800

    Added title for select tag

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -9,7 +9,7 @@ const ExpensesFilter = ({ selectedYear, onChangeYear }) => {
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select value={selectedYear} onChange={dropdownChangeHandler}>
+        <select title="yearSelector" value={selectedYear} onChange={dropdownChangeHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
